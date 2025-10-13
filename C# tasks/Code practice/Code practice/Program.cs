4740cad7c19d9a3d41b6580e788728c6d2b0fb5a
Xalidaa <hacizadexalide@gmail.com>
2025-10-13 16:21:59 +0400

    Add new folders called C# tasks and Code Practice

---
+++ C# tasks/Code practice/Code practice/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Code_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///* Task 1*/
+            //int number = 37;
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}
+
+            ///* Task 2*/
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine("FizzBuzz");
+            //    }
+            //    else if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            /* Task 3*/
+            int count = 0;
+
+            for (int i = 80; i>=0;i--)
+            {
+                if (i % 2 != 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+
+            /* Task 4*/
+
+            int sum = 0;
+            for (int i = 0; i <= 50; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum = sum + i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine(sum);
+
+            /* Task 5*/
+
+            int eded = 2859;
+            say = 0
+            while (eded > 0)
+            {
+                int b = eded % 10;
+                eded = eded / 10;
+                say++;
+            }
+            Console.WriteLine(say);
+
+
+            /* Task 6*/
+
+
+        }
+    }
+}
---
+++ C# tasks/Task in C#/Task in C#/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace Task_in_C_
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            // Task 1: Massivin elementlərini ekrana çap et
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Task 2: Massivdəki elementlərin cəmini tap
+
+            int sum = 0;
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach (int i in numbers)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+
+            // Task 3: Massivdəki ən böyük elementi tap
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int largest = numbers[0];
+            foreach (int i in numbers)
+            {
+                if (i > largest)
+                {
+                    largest = i;
+                }
+            }
+            Console.WriteLine(largest);
+
+            // Task 5: Massivdə cüt və tək ədədlərin sayını tap
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int cut = 0;
+            int tek = 0;
+            foreach (int i in numbers)
+            {
+                if (i % 2 == 0)
+                {
+                    cut = cut + 1;
+                }
+                else
+                {
+                    tek = tek + 1;
+                }
+
+            }
+            Console.WriteLine(cut);
+            Console.WriteLine(tek);
+
+            // Task 6:  Massivi tərsinə çap et
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int[] reversed = new int[numbers.Length];
+            for (int i = numbers.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                reversed[j] = numbers[i];
+            }
+            Console.WriteLine(string.Join(", ", reversed));
+
+            // Task 7: Massivdə konkret ədədin olub-olmadığını yoxla (Linear Search)
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int Axtarilan_eded = 6;
+            bool varmi = false;
+            foreach (int i in numbers)
+            {
+                if (i == Axtarilan_eded)
+                {
+                    varmi = true;
+                    break;
+                }
+            }
+            Console.WriteLine(varmi);
+
+            // Task 8: Massivdəki elementlərin orta qiymətini tap
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int sum = 0;
+            int count = 0;
+            foreach (int i in numbers)
+            {
+                sum = sum + i;
+                count++;
+            }
+            Console.WriteLine(sum / count);
+
+            // Task 9: İki massiv arasında ümumi elementləri tap
+
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 4, 5, 6, 7, 8 };
+            List<int> commonElements = new List<int>();
+            foreach (int i in array1)
+            {
+                foreach (int j in array2)
+                {
+                    if (i == j && !commonElements.Contains(i))
+                    {
+                        commonElements.Add(i);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", commonElements));
+        }
+    }
+}
