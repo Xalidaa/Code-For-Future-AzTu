072b6d7ac478e0442237cdc44ac05c1fe53fddad
Xalidaa <hacizadexalide@gmail.com>
2025-10-18 21:20:56 +0400

    C# lecture codes and comments

--- C# lecture codes/C# Intro/C# Intro/obj/Debug/net9.0/C# Intro.AssemblyInfo.cs
+++ C# lecture codes/C# Intro/C# Intro/obj/Debug/net9.0/C# Intro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# Intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df47d59bbbea4b298d9fb1c8fbdd87d5445b653a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dabd494a2de9786adb1c206df8a97e307a08f762")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# Intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# Intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
