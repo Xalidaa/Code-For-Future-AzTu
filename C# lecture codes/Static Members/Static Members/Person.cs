ba62e9fc3d3cb05f5fc4bb3208370a7b6ad3053c
Xalidaa <hacizadexalide@gmail.com>
2025-10-29 13:01:26 +0400

    Static members in C#

---
+++ C# lecture codes/Static Members/Static Members/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Members
+{
+    class Person
+    {
+        //public int instance_count = 0;
+        public static int instance_count = 0;
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public static int WorkAge { get; set; }
+
+        // class yarandigi anda static ctor ise dusur
+        // adi ctor ise obyekt yarananda ise dusur
+
+        //static ctoru static bir deyisene deyer menimsetmek isteyende istifade edirik
+        static Person()
+        {
+            WorkAge = 19;
+            Console.WriteLine("static ctor ise dusdu");
+        }
+
+        public Person ()
+        {
+            instance_count++; 
+            Console.WriteLine(instance_count);
+            // her obyekt ucun yeniden basdaki qiymete qayidir ele artirir eger non-staticdirse field; output olur: 1 1 1
+            //amma static olanda en son ne idise deyer ona elave edir output olur bu halda: 1 2 3
+            Console.WriteLine("non-static ctor ise dusdu");
+        }
+
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name}  Age: {Age} Surname: {Surname}");
+        }
+
+        //metodun obyektden hec bir asililigi yoxdur, bunu staticle etsek daha yaxsi olar
+        public static string Capitalize (string word)
+        {
+            return word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower();
+        }
+    }
+}
---
+++ C# lecture codes/Static Members/Static Members/Program.cs
@@ -0,0 +1,60 @@
+﻿// static members - classdan asili olan field, constructor, method ve property
+// non-static members - obyektden asili olan field, constructor, method ve property
+
+//Name, Surname, Age non-staticdir
+//WorkAge static memberdir
+
+//non-static memberler static memberlerden istifade ede bilir cunki static non-staticden qabaq yaranmisdi artiq
+
+
+namespace Static_Members
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person_1 = new Person
+            {
+                Name = "Ali",
+                Surname = "Aliyev",
+                Age = 20,
+
+            };
+
+            Person person_2 = new Person
+            {
+                Name = "Vali",
+                Surname = "Valiyev",
+                Age = 23,
+
+            };
+
+            Person person_3 = new Person
+            {
+                Name = "Hesen",
+                Surname = "Hesenov",
+                Age = 30,
+
+            };
+
+            Person person_4 = new Person
+            {
+                Name = "Hesen",
+                Surname = "Hesenov",
+                Age = 30,
+
+            };
+
+            ////Console.WriteLine(person_1.WorkAge); this is wrong
+
+            Console.WriteLine(Person.WorkAge);
+
+
+            person_1.GetInfo();
+            //Console.WriteLine(person_1.Capitalize("hello")); this is wrong
+            Console.WriteLine(Person.Capitalize("hello"));
+
+            Console.WriteLine(Person.instance_count);
+        }
+    }
+}
