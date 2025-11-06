0c99626319f819bd342a736381767b0e2125f85b
unknown <hacizadexalide@gmail.com>
2025-11-06 16:38:46 +0400

    Serialize and deserialize json files

---
+++ C# tasks/SerializeTasks/SerializeTasks/Program.cs
@@ -0,0 +1,28 @@
+﻿using SerializeTasks.Models;
+using SerializeTasks.Services;
+
+namespace SerializeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\ca r221.14\\source\\repos\\SerializeTasks\\SerializeTasks\\Data\\JSons\\index.json";
+            List<Student> students = new()
+            {
+                new Student {Name = "Narmin", Age =20 , Group = new Group {Name = "6523e" } },
+                new Student {Name = "Ulviyye", Age =21 , Group = new Group {Name = "652id" } }
+            };
+
+            StudentService.WriteStudentsToFile(path, students);
+            Console.WriteLine("Ilkin telebeler fayla yazildi.");
+
+            Student newStudent = new Student{Name = "Khalida", Age = 19, Group = new Group { Name = "APA201" } };
+            StudentService.AddStudentToFile(path, newStudent);
+            Console.WriteLine("Yeni telebe elave edildi");
+
+            StudentService.RemoveStudentFromFile(path, "Narmin");
+            Console.WriteLine($"Telebe silindi");
+        }
+    }
+}
---
+++ C# tasks/SerializeTasks/SerializeTasks/Services/StudentService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SerializeTasks.Models;
+using Newtonsoft.Json;
+
+namespace SerializeTasks.Services
+{
+    class StudentService
+    {
+        public static void WriteStudentsToFile(string path,List<Student> students )
+        {
+            string jsonString = JsonConvert.SerializeObject(students, Formatting.Indented);
+            using (StreamWriter sw = new StreamWriter(path,false))
+            {
+                sw.WriteLine(jsonString);
+            }
+        }
+
+        public static void AddStudentToFile(string path, Student student)
+        {
+                List<Student> students;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string fileContent = sr.ReadToEnd();
+
+                if (string.IsNullOrEmpty(fileContent)) 
+                {
+                    students = new List<Student>();
+                }
+                else
+                {
+                    students = JsonConvert.DeserializeObject<List<Student>>(fileContent);
+                    
+                }
+
+
+            }
+                students.Add(student);
+
+                WriteStudentsToFile(path, students);
+            
+        }
+        public static void RemoveStudentFromFile(string path, string studentName)
+        {
+            List<Student> students;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string fileContent = sr.ReadToEnd();
+                students = JsonConvert.DeserializeObject<List<Student>>(fileContent);
+
+                if (students.Contains(studentName))
+                {
+                    students.Remove(students.IndexOf(studentName));
+                }
+                else
+                {
+                    Console.WriteLine($"The student with name {studentName} is not found");
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                string jsonString = JsonConvert.SerializeObject(students, Formatting.Indented);
+                sw.WriteLine(jsonString);
+            }
+        }
+    }
+}
